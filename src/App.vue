dfa6f38ddb9d07f49941b4da7bc99118794de6e9
iYuan <yuan@iyuan.ltd>
2022-12-26 18:19:08 +0800

    V1

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,17 @@
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
+    <div class="navbar bg-white rounded-3xl shadow-xl">
+      <div class="flex-none">
+        <button class="btn btn-circle btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </button>
+      </div>
+      <div class="flex-1">
+        <RouterLink to="/" class="btn btn-ghost normal-case text-xl">🚀即刻发射台</RouterLink>
+      </div>
+    </div>
+    <nav></nav>
   </header>
 
   <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,43 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const essaytext = ref('');
+const buttonText1 = ref('发送');
+
+const submitText = ref(() => {
+  var btn = document.querySelector('.btn1');
+  btn.classList.add('loading');
+  axios
+    .get('https://essay-api.o0.work/send-essay?key=iYuanKeyTemporary', {
+      params: {
+        text: essaytext.value,
+        from: '即刻发射台',
+      },
+    })
+    .then(function (response) {
+      console.log(response);
+      if (response.status == 200) {
+        btn.classList.remove('loading');
+        btn.classList.add('btn-success');
+        buttonText1.value = '发送成功';
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+      btn.classList.remove('loading');
+      btn.classList.add('btn-error');
+      buttonText1.value = '发送失败';
+    });
+});
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="p-4 rounded-3xl shadow-xl mt-4 bg-white flex justify-center items-center flex-col">
+      <div class="bg-clip-text text-transparent bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 font-bold text-3xl mb-4">文字版即刻发送</div>
+      <input type="text" placeholder="占位符qwq" class="input input-bordered input-primary w-full h-[12rem] mb-4" v-model="essaytext" />
+      <button class="btn btn-primary w-full btn1" @click="submitText">{{ buttonText1 }}</button>
+    </div>
+  </main>
 </template>
